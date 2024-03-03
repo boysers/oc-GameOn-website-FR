d2a05ce78a03219def855d6a2fb2a2d9675a3f7b
boysers <boyser2015@gmail.com>
2024-03-03 17:34:39 +0100

    feat(validator): is date of birth

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -2,111 +2,110 @@ import { Validator } from "../utils/Validator";
 import { UserEntityException } from "./UserEntityException";
 
 interface IUserProps {
-	firstname: string;
-	lastname: string;
-	email: string;
-	birthdate: string;
-	quantity: number;
-	location: string;
-	checkbox1: boolean;
-	checkbox2: boolean;
+  firstname: string;
+  lastname: string;
+  email: string;
+  birthdate: string;
+  quantity: number;
+  location: string;
+  checkbox1: boolean;
+  checkbox2: boolean;
 }
 
 export class User {
-	static createUser(props: IUserProps) {
-		const user = new User(props);
-		user.validate();
-		return user;
-	}
+  static createUser(props: IUserProps) {
+    const user = new User(props);
+    user.validate();
+    return user;
+  }
 
-	private constructor(private props: IUserProps) {}
+  private constructor(private props: IUserProps) {}
 
-	private validate() {
-		const rules = {
-			firstname: {
-				validationFunction: (name: string) => Validator.isName(name),
-				errorMessage:
-					"Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
-			},
-			lastname: {
-				validationFunction: (name: string) => Validator.isName(name),
-				errorMessage:
-					"Veuillez entrer 2 caractères ou plus pour le champ du nom.",
-			},
-			email: {
-				validationFunction: (email: string) =>
-					Validator.isEmailAddress(email),
-				errorMessage: "Veuillez entrer un email valide.",
-			},
-			birthdate: {
-				validationFunction: (date: string) => Validator.isDate(date),
-				errorMessage: "Vous devez entrer votre date de naissance.",
-			},
-			quantity: {
-				validationFunction: (quantity: string) =>
-					Validator.isQuantityNumber(quantity),
-				errorMessage:
-					"Veuillez entrer le nombre de tournois GameOn auxquels vous avez déjà participé.",
-			},
-			location: {
-				validationFunction: (location: string) => !!location,
-				errorMessage: "Vous devez choisir une option.",
-			},
-			checkbox1: {
-				validationFunction: (checked: boolean) => checked,
-				errorMessage:
-					"Vous devez vérifier que vous acceptez les termes et conditions.",
-			},
-		};
+  private validate() {
+    const rules = {
+      firstname: {
+        validationFunction: (name: string) => Validator.isName(name),
+        errorMessage:
+          "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
+      },
+      lastname: {
+        validationFunction: (name: string) => Validator.isName(name),
+        errorMessage:
+          "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+      },
+      email: {
+        validationFunction: (email: string) => Validator.isEmailAddress(email),
+        errorMessage: "Veuillez entrer un email valide.",
+      },
+      birthdate: {
+        validationFunction: (date: string) => Validator.isDateOfBirth(date),
+        errorMessage: "Vous devez entrer votre date de naissance.",
+      },
+      quantity: {
+        validationFunction: (quantity: string) =>
+          Validator.isQuantityNumber(quantity),
+        errorMessage:
+          "Veuillez entrer le nombre de tournois GameOn auxquels vous avez déjà participé.",
+      },
+      location: {
+        validationFunction: (location: string) => !!location,
+        errorMessage: "Vous devez choisir une option.",
+      },
+      checkbox1: {
+        validationFunction: (checked: boolean) => checked,
+        errorMessage:
+          "Vous devez vérifier que vous acceptez les termes et conditions.",
+      },
+    };
 
-		const invalidInput = {};
+    const invalidInput = {};
 
-		Object.entries(this.snapshot).forEach(([key, values]) => {
-			if (!rules[key]) return;
-			const { validationFunction, errorMessage } = rules[key];
-			if (!validationFunction(values)) {
-				invalidInput[key] = errorMessage;
-			}
-		});
+    Object.entries(this.snapshot).forEach(([key, values]) => {
+      if (!rules[key]) return;
+      const { validationFunction, errorMessage } = rules[key];
+      if (!validationFunction(values)) {
+        invalidInput[key] = errorMessage;
+      }
+    });
 
-		if (Object.keys(invalidInput).length > 0) {
-			throw new UserEntityException(invalidInput);
-		}
-	}
+    if (Object.keys(invalidInput).length > 0) {
+      throw new UserEntityException(invalidInput);
+    }
+  }
 
-	get snapshot() {
-		return this.props;
-	}
+  get snapshot() {
+    return this.props;
+  }
 
-	set firstname(first: string) {
-		this.props.firstname = first;
-	}
+  set firstname(first: string) {
+    this.props.firstname = first;
+  }
 
-	set lastname(last: string) {
-		this.props.lastname = last;
-	}
+  set lastname(last: string) {
+    this.props.lastname = last;
+  }
 
-	set email(email: string) {
-		this.props.email = email;
-	}
+  set email(email: string) {
+    this.props.email = email;
+  }
 
-	set birthdate(date: string) {
-		this.props.birthdate = date;
-	}
+  set birthdate(date: string) {
+    this.props.birthdate = date;
+  }
 
-	set quantity(quantity: number) {
-		this.props.quantity = quantity;
-	}
+  set quantity(quantity: number) {
+    this.props.quantity = quantity;
+  }
 
-	set location(location: string) {
-		this.props.location = location;
-	}
+  set location(location: string) {
+    this.props.location = location;
+  }
 
-	set checkbox1(checked: boolean) {
-		this.props.checkbox1 = checked;
-	}
+  set checkbox1(checked: boolean) {
+    this.props.checkbox1 = checked;
+  }
 
-	set checkbox2(checked: boolean) {
-		this.props.checkbox2 = checked;
-	}
+  set checkbox2(checked: boolean) {
+    this.props.checkbox2 = checked;
+  }
 }
--- src/utils/Validator.ts
+++ src/utils/Validator.ts
@@ -1,26 +1,42 @@
 export class Validator {
-	static isName(name: string): boolean {
-		if (name?.trim().length < 2) return false;
-		return true;
-	}
+  static isName(name: string): boolean {
+    if (typeof name !== "string") return false;
+    const trimmedName = name.trim();
+    const isNonEmptyString = trimmedName.length >= 2;
+    const containsOnlyLetters = /^[a-zA-Z]+$/.test(trimmedName);
+    return isNonEmptyString && containsOnlyLetters;
+  }
 
-	static isEmailAddress(email: string): boolean {
-		const emailRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-		return emailRegExp.test(email);
-	}
+  static isEmailAddress(email: string): boolean {
+    const emailRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegExp.test(email);
+  }
 
-	static isDate(dateString: string): boolean {
-		const date = new Date(dateString);
-		return !isNaN(date.getTime());
-	}
+  static isDate(dateString: string): boolean {
+    const date = new Date(dateString);
+    return !isNaN(date.getTime());
+  }
 
-	static isQuantityNumber(
-		quantity: string,
-		min: number = 0,
-		max: number = 99
-	): boolean {
-		const quantityValue = parseInt(quantity, 10);
-		const isWithinRange = quantityValue >= min && quantityValue <= max;
-		return !isNaN(quantityValue) && isWithinRange;
-	}
+  static isQuantityNumber(
+    quantity: string,
+    min: number = 0,
+    max: number = 99
+  ): boolean {
+    const quantityValue = parseInt(quantity, 10);
+    const isWithinRange = quantityValue >= min && quantityValue <= max;
+    return !isNaN(quantityValue) && isWithinRange;
+  }
+
+  static isDateOfBirth(dateString) {
+    const date = new Date(dateString);
+    const currentYear = new Date().getFullYear();
+    const minimumBirthYear = currentYear - 120;
+    const maximumBirthYear = currentYear - 10;
+
+    return (
+      !isNaN(date.getTime()) &&
+      date.getFullYear() >= minimumBirthYear &&
+      date.getFullYear() <= maximumBirthYear
+    );
+  }
 }
